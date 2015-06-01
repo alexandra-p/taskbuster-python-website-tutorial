5f18a4bf94b178acceb12a2ba922e14f7c49ae42
Alexandra <ale@itnovate.ro>
2015-06-01 21:07:01 +0200

    sets path to look for static files

--- taskbuster/settings/base.py
+++ taskbuster/settings/base.py
@@ -107,3 +107,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+# This line tells Django to look for static files in a folder named static inside each of our apps.
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
